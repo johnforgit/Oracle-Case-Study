a514ed2ba957c0b9ee59f8422ed4750507994254
SIVADATH K M <sivadath.km.oracle@gmail.com>
2025-07-17 12:21:37 +0530

    Project Initialization

---
+++ ALMS/src/main/java/com/oracle/AlmsApplication.java
@@ -0,0 +1,13 @@
+package com.oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmsApplication.class, args);
+	}
+
+}
