d07eb2c9d32b1df9a78bea3f83e9a02ca82a5acb
sera-101 <sera.jack101@gmail.com>
2025-07-21 17:18:28 +0530

    lombok error fixed

--- ALMS/src/main/java/com/oracle/dto/DurationGapDTO.java
+++ ALMS/src/main/java/com/oracle/dto/DurationGapDTO.java
@@ -5,7 +5,9 @@ import java.util.Objects;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+@Data
 public class DurationGapDTO {
 
     @Schema(description = "Computed duration gap", example = "1.3476")
@@ -19,29 +21,4 @@ public class DurationGapDTO {
         this.durationGap = durationGap;
     }
 
-    public BigDecimal getDurationGap() {
-        return durationGap;
-    }
-
-    public void setDurationGap(BigDecimal durationGap) {
-        this.durationGap = durationGap;
-    }
-
-    @Override
-    public String toString() {
-        return "DurationGapDTO [durationGap=" + durationGap + "]";
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(durationGap);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        DurationGapDTO other = (DurationGapDTO) obj;
-        return Objects.equals(durationGap, other.durationGap);
-    }
 }
